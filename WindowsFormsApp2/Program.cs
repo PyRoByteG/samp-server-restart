df4ee6599b3e045560c29d2d7e3252ff3ac80785
PyRoByteG <69804863+PyRoByteG@users.noreply.github.com>
2020-08-28 13:44:24 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+//using System.Keys
+namespace WindowsFormsApp2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            foreach (var process in Process.GetProcessesByName("samp-server"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("samp-npc"))
+            {
+                process.Kill();
+            }
+            Process.Start("samp-server.exe");
+            Process.Start("samp-npc.exe");
+
+            Application.Exit();
+        }
+    }
+}
